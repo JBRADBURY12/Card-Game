512207d99e30db5ffc421ce046ac73211f10f0c4
Jacob Jackson <143200043+JacobJ8210@users.noreply.github.com>
2024-12-21 02:44:09 -0500

    Implemented Most of the Character Controller
    -moving
    -looking around
    -sprinting
    -jumping

--- Card Game/Assets/FirstPersonController.cs
+++ Card Game/Assets/FirstPersonController.cs
@@ -2,15 +2,79 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintMultiplier = 2.0f;
+
+    [Header("Jump Parameters")]
+    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float gravity = 9.81f;
+
+    [Header("Look Sensitivity")]
+    [SerializeField] private float mouseSensitivity = 2.0f;
+    [SerializeField] private float upDownRange = 80.0f;
+
+    [Header("Inputs Customisation")]
+    [SerializeField] private string horizontalMoveInput = "Horizontal";
+    [SerializeField] private string verticalMoveInput = "Vertical";
+
+    [SerializeField] private string MouseXInput = "Mouse X";
+    [SerializeField] private string MouseYInput = "Mouse Y";
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+
+
+    private Camera mainCamera;
+    private float verticalRotation;
+    private Vector3 currentMovement = Vector3.zero;
+    private CharacterController characterController;
+
+    private void Start() {
+        characterController = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    void HandleMovement()
     {
+        float speedMultiplier = Input.GetKey(sprintKey) ? sprintMultiplier : 1f;
+
+        // Use Input.GetAxisRaw or the character will keep moving a few frames after button release.
+        Vector3 horizontalMovement = new Vector3(Input.GetAxisRaw(horizontalMoveInput), 0, Input.GetAxisRaw(verticalMoveInput)).normalized;
+        horizontalMovement = transform.rotation * horizontalMovement;
         
+        HandleGravityAndJumping();
+
+        currentMovement.x = horizontalMovement.x * walkSpeed * speedMultiplier;
+        currentMovement.z = horizontalMovement.z * walkSpeed * speedMultiplier;
+        
+        characterController.Move(currentMovement  * Time.deltaTime);
+    }
+
+    void HandleGravityAndJumping() {
+        if(characterController.isGrounded) {
+            currentMovement.y = -0.5f;
+
+            if(Input.GetKeyDown(jumpKey)) {
+                currentMovement.y = jumpForce;
+            }
+        } else {
+            currentMovement.y -= gravity * Time.deltaTime;
+        }
+    }
+
+    void HandleRotation() {
+        float mouseXRotation = Input.GetAxis(MouseXInput) * mouseSensitivity;
+        transform.Rotate(0, mouseXRotation, 0);
+
+        verticalRotation -= Input.GetAxis(MouseYInput) * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
     }
 }
